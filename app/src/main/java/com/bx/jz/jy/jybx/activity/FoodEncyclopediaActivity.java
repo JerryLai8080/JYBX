d4572afdb966a89bd13d0f7f1999e875de5a9dfb
Torando1015 <760982661@qq.com>
2017-11-10 14:38:10 +0800

    优化recycleview1

--- app/src/main/java/com/bx/jz/jy/jybx/activity/FoodEncyclopediaActivity.java
+++ app/src/main/java/com/bx/jz/jy/jybx/activity/FoodEncyclopediaActivity.java
@@ -13,6 +13,5 @@ public class FoodEncyclopediaActivity extends BaseActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
     }
 }
